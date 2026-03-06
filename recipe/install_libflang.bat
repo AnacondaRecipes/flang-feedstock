19b98a542a85c046ac949a4b621019107ebe0f38
Eric Lundby <Eric.Lundby@gmail.com>
2026-03-06 09:08:19 -0700

    Update install_libflang.bat

--- recipe/install_libflang.bat
+++ recipe/install_libflang.bat
@@ -1,4 +1,4 @@
 @echo on
 
-cp build\lib\FortranRuntime.lib %LIBRARY_LIB%
-cp build\lib\FortranDecimal.lib %LIBRARY_LIB%
+copy /Y "build\lib\FortranRuntime.lib" "%LIBRARY_LIB%\"
+copy /Y "build\lib\FortranDecimal.lib" "%LIBRARY_LIB%\"
